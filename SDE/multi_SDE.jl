b83a25da56eefbcb579b623dc404691725c12510
Michelle Ko <michelle.ko@uwaterloo.ca>
2021-05-02 11:26:49 -0400

    Include nonnegativity condition & add comments

--- SDE/multi_SDE.jl
+++ SDE/multi_SDE.jl
@@ -7,15 +7,17 @@ using Random
 @model Euler_SDE(Xt, dt, ninter, npar, mu, sigma) = begin
     # Memory allocation
 	nagent, nobs = size(Xt)
-    theta = Vector{Real}(undef, npar)
+    theta = tzeros(Float64, npar)
     Xlat = Array{Real}(undef, (nagent, nobs-1, ninter))
     dtl = dt/(ninter+1)
 
-    # Prior distribution of theta (variance of 5 is completely arbitrary)
-	#theta ~ MvNormal(zeros(npar), 5)
-	theta[1] ~ Uniform(0.2,0.9)
-	theta[2] ~ Uniform(0.001, 0.030)
-	theta[3] ~ Uniform(0.1,0.6)
+    # Prior distribution of theta (variance is completely arbitrary)
+	# theta ~ MvNormal(zeros(npar), 5)
+
+	# Specific to Lotka-Volterra
+	theta[1] ~ Uniform(0.1,1)
+	theta[2] ~ Uniform(0.001, 0.05)
+	theta[3] ~ Uniform(0.1,1)
 
     for i in 2:nobs
         # Latent variables
@@ -51,16 +53,17 @@ function sigma(X, theta)
 	[[theta[1]*X[1] + theta[2]*X[1]*X[2], - theta[2]*X[1]*X[2]] [- theta[2]*X[1]*X[2], theta[2]*X[1]*X[2] + theta[3]*X[2]]]
 end
 
+
+# Custom Multivariate Normal to deal with negative values and nonposdef matrices
 struct CustomMvN <: ContinuousMultivariateDistribution
 	μ::Array
 	Σ::Array
-
 end
 
+# Components required for sampling
 Distributions.length(d::CustomMvN) = length(d.μ)
-Distributions._rand!(rng::Random._GLOBAL_RNG, d::CustomMvN, x::Array{Float64,1}) = isposdef(d.Σ) ? x = rand(MvNormal(d.μ, d.Σ)) : x = zero(x)
-Distributions._logpdf(d::CustomMvN, x::Array{Float64,1}) = isposdef(d.Σ) ? logpdf(MvNormal(d.μ, d.Σ), x) : -Inf
-
+Distributions._rand!(rng::Random._GLOBAL_RNG, d::CustomMvN, x::Array{Float64,1}) = (sum(d.μ .> 0) == length(d.μ)) & isposdef(d.Σ) ? x = rand(MvNormal(d.μ, d.Σ)) : x = zero(x)
+Distributions._logpdf(d::CustomMvN, x::Array{Float64,1}) = (sum(d.μ .> 0) == length(d.μ)) & isposdef(d.Σ) ? logpdf(MvNormal(d.μ, d.Σ), x) : -Inf
 
 """
 Testing!
@@ -74,13 +77,11 @@ With 11 observations, 4 subintervals, 50 particles and 1000 iterations,
 PG() completes sampling after 1.5 ~ 2 minutes.
 """
 
-
 c = [0.5, 0.0025, 0.3]
 X0 = [100.0, 100.0]
 
 t_path, X_path = Gillespie(c,X0,HazardFun,StoichMatrix,0.0,20.0)
 t, Xt = DiscretizePath(t_path,X_path,1,20)
-plot(t, Xt[1,:])
 
 dt = 1
 ninter = 4
@@ -97,6 +98,11 @@ chn_NutsGibbs = sample(Euler_SDE(Xt, dt, ninter, npar, mu, sigma),
 
 estims = Array(chn)
 
+estims[500,:]
+
 plot(estims[:,end-2])
-plot!(estims[:,end-1])
-plot!(estims[:,end])
+plot(estims[:,end-1])
+plot(estims[:,end])
+
+plot(t_path, X_path[2,:])
+plot!(range(0,t_path[end], length=length(estims[iter,1:2:end-3])), estims[iter,2:2:end-3])
